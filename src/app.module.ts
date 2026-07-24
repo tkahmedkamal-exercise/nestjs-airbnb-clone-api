fd696353daca6515f87528597e420c5c63dba131
tkahmedkamal-exercise <t.ahmed.elgamal22@gmail.com>
2026-07-25 00:05:32 +0300

    Refactor code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,39 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { envSchema } from './common/config/env-schema.validation';
-import configMapping from './common/config/config-mapping';
-import {
-  AcceptLanguageResolver,
-  HeaderResolver,
-  I18nModule,
-  QueryResolver,
-} from 'nestjs-i18n';
-import path from 'node:path';
 import { CustomI18nService } from './i18n/custom-i18n.service';
+import { CoreModule } from './core.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validationSchema: envSchema,
-      load: [configMapping],
-    }),
-    I18nModule.forRoot({
-      fallbackLanguage: 'en',
-      loaderOptions: {
-        path: path.join(__dirname, '/i18n/'),
-        watch: true,
-      },
-      resolvers: [
-        { use: QueryResolver, options: ['lang'] },
-        AcceptLanguageResolver,
-        new HeaderResolver(['x-lang']),
-      ],
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService, CustomI18nService],
+  imports: [CoreModule],
+  providers: [CustomI18nService],
 })
 export class AppModule {}
--- src/common/config/env-modes/default.env.ts
+++ src/common/config/env-modes/default.env.ts
@@ -2,4 +2,5 @@ import { Environment } from '../env.interface';
 
 export const defaultEnv = (): Environment => ({
   port: Number(process.env.PORT),
+  fullbackLanguage: process.env.FALLBACK_LANGUAGE!,
 });
